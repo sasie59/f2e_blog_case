7c60c797545ec61c09b9d5ba821f4556cd4e6a15
sasie59 <sasie59@gmail.com>
2022-03-13 19:02:09 +0800

    header 元件完成 (but手機版 searchBar的button寬度會變動? )桌機 平板 手機完成

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -11,14 +11,20 @@ export function Header() {
       <div className={style.menu}>
         <div className={style.searchBar}>
           <input placeholder='搜尋視覺或專案...' type="text" />
+          <div className={style.space} />
           <button />
         </div>
-        <div className={style.proposal}>
-          <div className={style.img} />
-          <img src={pen} alt="" />
-          <div className={style.text}>投稿/提案</div>
+        <div className={style.include}>
+          <div className={style.proposal}>
+            <div className={style.space1} />
+            <div className={style.img} />
+            <div className={style.space2} />
+            <div className={style.text}>投稿/提案</div>
+          </div>
         </div>
+        <div className={style.space} />
         <div className={style.photo} />
+        <div className={style.lastSpace} />
       </div>
     </div>
   )
