c0ebed41037f728d1eb5cbd721fe6c1fb9c3ca79
sasie59 <sasie59@gmail.com>
2022-04-02 14:51:01 +0800

    存檔一下

--- src/pages/Contribute/index.js
+++ src/pages/Contribute/index.js
@@ -1,27 +1,44 @@
 import React from 'react'
 import style from './style.module.scss';
 import * as Comp from '../../components';
-
+import addImg from './addImg.png';
 export function Contribute() {
   return (
     <div className={style.layout}>
       <Comp.Header />
       <div className={style.Contribute}>
         <div className={style.firstRow}>
-          {/* <Comp.Combobox
-
-          /> */}
-          <div>
-            <img src="" alt="" />
-            <div></div>
+          <Comp.Combobox className={style.myCombobox1} list={['請 選 擇', 'UI 投搞', '提     案']} />
+          <div className={style.photo}>
+            <img src={addImg} alt="" />
+            <div className={style.addImg}>+ 新增圖片</div>
+            <div className={style.imgDescribe}>視覺或示意圖<br />(建議
+              <span style={{ color: '#00A3FF' }}>
+                寬670 x 高502
+              </span>
+              JPG、PNG)
+            </div>
           </div>
         </div>
         <div className={style.secRow}>
-          {/* <Comp.Combobox
-
-          /> */}
+          <div className={style.secDescribe}>創用CC
+            <span style={{ color: '#00A3FF' }}>
+              (版權說明)
+            </span>
+            <span style={{ color: '#FF688C' }}>
+              *必填
+            </span>
+          </div>
+          <Comp.Combobox className={style.myCombobox2} list={['請 選 擇', '姓名標示', '姓名標示－非商業性']} />
         </div>
-        <div className={style.name}></div>
+        <div className={style.name}>
+          <div className={style.nameDescribe}>專案名稱
+            <span style={{ color: '#FF688C' }}>
+              *必填
+            </span>
+          </div>
+          <input type="text" />
+        </div>
         <div className={style.types}></div>
         <div className={style.fileLink}></div>
         <div className={style.describe}></div>
