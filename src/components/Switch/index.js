eda149a33e768d664c0f8c089ded4cfc5d668fd7
sasie59 <sasie59@gmail.com>
2022-04-09 00:15:07 +0800

    改寫

--- src/components/Switch/index.js
+++ src/components/Switch/index.js
@@ -1,30 +1,17 @@
-import React, { useState } from 'react'
+import React from 'react'
 import style from './style.module.scss';
 
 export function Switch(props) {
-  const [choice, setChoice] = useState('proposal');
-
-  const handleClick = (name) => {
-    setChoice(name);
-    // console.warn(name);
-  }
-
   return (
     <div className={style.Switch}>
-      <div
-        name='design'
-        onClick={handleClick.bind(this, 'design')}
-        className={choice === 'design' ? style.design : style.ngised}
-      >
-        {props.design}
-      </div>
-      <div
-        onClick={handleClick.bind(this, 'proposal')}
-        name='proposal'
-        className={choice === 'proposal' ? style.lasoporp : style.proposal}
-      >
-        {props.proposal}
-      </div>
+      {props.tabList.map(item =>
+        <div
+          onClick={props.onChange.bind(this, item.key)}
+          className={props.choice === item.key ? style.active : ''}
+        >
+          {item.text}
+        </div>
+      )}
     </div>
   )
 }
--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -12,28 +12,35 @@ import pen from './Icon/pen.png';
 const text = '開啟定位，搜尋附近藝文活動與台灣景點，簡單快速的規劃功能，輕鬆安排旅程...';
 const text2 = '想做一支整合股市、基金等資訊的網站，除了分析股市，也加入各家銀行手續…';
 const imgArr = [
-  { head: head, photo: photo },
-  { head: head, photo: photo },
-  { head: head, photo: photo },
-  { head: head, photo: photo },
-  { head: head, photo: photo },
-  { head: head, photo: photo },
-  { head: head, photo: photo },
-  { head: head, photo: photo },
-  { head: head, photo: photo },
-  { head: head, photo: photo },
-  { head: head, photo: photo },
-  { head: head, photo: photo },
-  { head: head, photo: photo },
-  { head: head, photo: photo },
-  { head: head, photo: photo },
+  { name: "aaa", head: head, photo: photo, type: 'proposal' },
+  { name: "bbb", head: head, photo: photo, type: 'design' },
+  { name: "ccc", head: head, photo: photo, type: 'proposal' },
+  { name: "ddd", head: head, photo: photo, type: 'design' },
+  { name: "eee", head: head, photo: photo, type: 'proposal' },
+  { name: "fff", head: head, photo: photo, type: 'design' },
+  { name: "ggg", head: head, photo: photo, type: 'proposal' },
+  { name: "hhh", head: head, photo: photo, type: 'design' },
+  { name: "iii", head: head, photo: photo, type: 'proposal' },
+  { name: "jjj", head: head, photo: photo, type: 'design' },
+  { name: "kkk", head: head, photo: photo, type: 'proposal' },
+  { name: "lll", head: head, photo: photo, type: 'aaa' },
+  { name: "mmm", head: head, photo: photo, type: 'bbb' },
+  { name: "nnn", head: head, photo: photo, type: 'aaa' },
+  { name: "ooo", head: head, photo: photo, type: 'bbb' },
 ]
 export function HomePage() {
   const [isChange, setIsChange] = useState(true);
+  const [isAppear, setIsAppear] = useState(false);
+  const [choice, setChoice] = useState("proposal");
 
   const handleClick = () => {
-    setIsChange(!isChange)
+    setIsAppear(!isAppear);
   }
+
+  const handleChange = (value) => {
+    setChoice(value);
+  }
+
   return (
     <div className={style.layout}>
       <Comp.Header />
@@ -41,23 +48,40 @@ export function HomePage() {
         <Comp.Switch
           design='設計分享'
           proposal='提案'
-          onClick={handleClick}
+          tabList={[
+            { key: 'design', text: '設計分享' },
+            { key: 'proposal', text: '提案' },
+            { key: 'aaa', text: 'AAA' },
+            { key: 'bbb', text: 'BBB' },
+          ]}
+          choice={choice}
+          onChange={handleChange}
         // isChange={isChange}
         />
         <div className={style.space} />
         <div className={style.newest}>
-          <div style={{ width: '61px', textAlignLast: 'justify' }}>最新</div>
-          <img src={arrow} alt="" />
+          {/* <div style={{ width: '65px', textAlignLast: 'justify' }}>
+            {isAppear ? '人氣瀏覽' : '最 新'}</div> */}
+          <Comp.BlackBox
+            list={['最         新', '人氣瀏覽']}
+          />
+          {/* <img
+            src={arrow}
+            alt=""
+            onClick={handleClick}
+          /> */}
         </div>
       </div>
       <div className={style.content}>
         <div className={style.item}>
-          {imgArr.map(item =>
+          {imgArr.filter(item =>
+            item.type === choice
+          ).map(item =>
             <a href='#/project'>
               <Comp.Card
                 head={isChange ? item.head : head2}
                 photo={isChange ? item.photo : photo2}
-                name={isChange ? 'Doris Wang' : 'Jenny Wilson'}
+                name={item.name}
                 title={isChange ? '我的輕旅行' : '股市分析'}
                 innerText={isChange ? text : text2}
               />
