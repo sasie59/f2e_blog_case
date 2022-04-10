0ed27d3ca71563431a933629c4a4d8fea407c6a7
sasie59 <sasie59@gmail.com>
2022-04-10 22:22:34 +0800

    component 之間的互動

--- src/components/BlackBox/index.js
+++ src/components/BlackBox/index.js
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import style from './style.module.scss';
-
+import arrow from './arrow.png';
 export function BlackBox(props) {
+  const [item,] = useState(props.list[0]);
+  const [isAppear, setIsAppear] = useState(false);
+
+  const handleClick = () => {
+    setIsAppear(!isAppear);
+  }
   return (
-    <div className={style.BlackBox}>
-      <div>
-        {props.news}
+    <div className={style.BlackBox} onClick={handleClick}>
+      <div className={style.choice}>
+        {item.text}
+        <img src={arrow} alt="" />
       </div>
-      <div>
-        {props.popular}
+      <div className={style.box}>
+        {isAppear && props.list.map(item =>
+          <div
+            className={style.selectOne}
+            key={item}
+            onClick={props.onChange.bind(this, item.key)}
+          >
+            {item.text}
+          </div>
+        )}
       </div>
-      <div className={style.logOut}>
-        {props.logOut}
-      </div>
+
     </div>
   )
 }
--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import style from './style.module.scss';
 import * as Comp from '../../components';
-import arrow from './arrow.png';
 import prev from './prev.png';
 import next from './next.png';
 import head from '../../components/Card/head.png';
@@ -11,34 +10,33 @@ import photo2 from '../../components/Card/photo2.png';
 import pen from './Icon/pen.png';
 const text = '開啟定位，搜尋附近藝文活動與台灣景點，簡單快速的規劃功能，輕鬆安排旅程...';
 const text2 = '想做一支整合股市、基金等資訊的網站，除了分析股市，也加入各家銀行手續…';
+const text3 = '大景。筠不，有產！轉到問我？都個一渝鳳人齊市之吝伙的碩都蚊的版林表下陳...'
 const imgArr = [
-  { name: "aaa", head: head, photo: photo, type: 'proposal' },
-  { name: "bbb", head: head, photo: photo, type: 'design' },
-  { name: "ccc", head: head, photo: photo, type: 'proposal' },
-  { name: "ddd", head: head, photo: photo, type: 'design' },
-  { name: "eee", head: head, photo: photo, type: 'proposal' },
-  { name: "fff", head: head, photo: photo, type: 'design' },
-  { name: "ggg", head: head, photo: photo, type: 'proposal' },
-  { name: "hhh", head: head, photo: photo, type: 'design' },
-  { name: "iii", head: head, photo: photo, type: 'proposal' },
-  { name: "jjj", head: head, photo: photo, type: 'design' },
-  { name: "kkk", head: head, photo: photo, type: 'proposal' },
-  { name: "lll", head: head, photo: photo, type: 'aaa' },
-  { name: "mmm", head: head, photo: photo, type: 'bbb' },
-  { name: "nnn", head: head, photo: photo, type: 'aaa' },
-  { name: "ooo", head: head, photo: photo, type: 'bbb' },
+  { head: head, photo: photo, type: 'design', title: '我的輕旅行', text: text, category: 'newest', name: "Doris Wang", },
+  { head: head, photo: photo, type: 'design', title: '我的輕旅行', text: text, category: 'popular', name: "Doris Wang", },
+  { head: head, photo: photo, type: 'design', title: '我的輕旅行', text: text, category: 'newest', name: "Doris Wang", },
+  { head: head, photo: photo, type: 'design', title: '我的輕旅行', text: text, category: 'newest', name: "Doris Wang", },
+  { head: head, photo: photo, type: 'design', title: '我的輕旅行', text: text, category: 'popular', name: "Doris Wang", },
+  { head: head, photo: photo, type: 'design', title: '我的輕旅行', text: text, category: 'popular', name: "Doris Wang", },
+  { head: head, photo: photo, type: 'design', title: '我的輕旅行', text: text, category: 'newest', name: "Doris Wang", },
+  { head: head, photo: photo, type: 'design', title: '我的輕旅行', text: text, category: 'newest', name: "Doris Wang", },
+  { head: head2, photo: photo2, type: 'proposal', title: '潤起有理人', text: text3, category: 'newest', name: "Jenny Wilson", },
+  { head: head2, photo: photo2, type: 'proposal', title: '與怠電了氧賺', text: text3, category: 'popular', name: "Bessie Cooper", },
+  { head: head2, photo: photo2, type: 'proposal', title: '研弟給要的年要觸完當', text: text3, category: 'newest', name: "Arlene McCoy", },
+  { head: head2, photo: photo2, type: 'proposal', title: '潤起有理人', text: text3, category: 'popular', name: "Robert Fox", },
+  { head: head2, photo: photo2, type: 'proposal', title: '股市分析', text: text2, category: 'newest', name: "高祐昀", },
+  { head: head2, photo: photo2, type: 'proposal', title: '股市分析', text: text2, category: 'newest', name: "郭輝聖", },
+  { head: head2, photo: photo2, type: 'proposal', title: '股市分析', text: text2, category: 'popular', name: "簡能銘", },
 ]
 export function HomePage() {
-  const [isChange, setIsChange] = useState(true);
-  const [isAppear, setIsAppear] = useState(false);
   const [choice, setChoice] = useState("proposal");
-
-  const handleClick = () => {
-    setIsAppear(!isAppear);
-  }
+  const [choose, setChoose] = useState('newest');
 
   const handleChange = (value) => {
     setChoice(value);
+  }
+  const handleChange2 = (value) => {
+    setChoose(value);
   }
 
   return (
@@ -51,39 +49,36 @@ export function HomePage() {
           tabList={[
             { key: 'design', text: '設計分享' },
             { key: 'proposal', text: '提案' },
-            { key: 'aaa', text: 'AAA' },
-            { key: 'bbb', text: 'BBB' },
           ]}
           choice={choice}
           onChange={handleChange}
-        // isChange={isChange}
         />
         <div className={style.space} />
         <div className={style.newest}>
-          {/* <div style={{ width: '65px', textAlignLast: 'justify' }}>
-            {isAppear ? '人氣瀏覽' : '最 新'}</div> */}
           <Comp.BlackBox
-            list={['最         新', '人氣瀏覽']}
+            list={[
+              { key: 'newest', text: '最 新' },
+              { key: 'popular', text: '人氣瀏覽' }
+            ]}
+            choose={choose}
+            onChange={handleChange2}
           />
-          {/* <img
-            src={arrow}
-            alt=""
-            onClick={handleClick}
-          /> */}
         </div>
       </div>
       <div className={style.content}>
         <div className={style.item}>
           {imgArr.filter(item =>
             item.type === choice
+          ).filter(item =>
+            item.category === choose
           ).map(item =>
             <a href='#/project'>
               <Comp.Card
-                head={isChange ? item.head : head2}
-                photo={isChange ? item.photo : photo2}
+                head={item.head}
+                photo={item.photo}
                 name={item.name}
-                title={isChange ? '我的輕旅行' : '股市分析'}
-                innerText={isChange ? text : text2}
+                title={item.title}
+                innerText={item.text}
               />
             </a>
           )}
